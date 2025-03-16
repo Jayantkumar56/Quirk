27a822beaf3f2babcaa0f948fcd6b873b7fbe93a
Jayant Singh <jayantkumar1890@gmail.com>
2025-03-16 20:37:52 +0530

    include errors removed

--- Quirk/src/Core/Frame/FontManager.h
+++ Quirk/src/Core/Frame/FontManager.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <array>
 
 namespace Quirk {
 
--- Quirk/src/Platform/Windows/WindowsTime.h
+++ Quirk/src/Platform/Windows/WindowsTime.h
@@ -4,6 +4,13 @@
 
 #ifdef QK_PLATFORM_WINDOWS
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // !WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <Windows.h>
+
 namespace Quirk {
 
 	class NativeTime {
